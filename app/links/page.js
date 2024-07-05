d17fd8b8640c2239c2b706bb596981251a41f1e7
AlgebraIbrahim <algebraibrahim@gmail.com>
2024-07-05 10:27:41 +0700

    initial commit

---
+++ app/links/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Snippet } from "@nextui-org/snippet";
+
+export default function Links() {
+  return (
+    <div>
+      <Snippet variant="shadow" color="secondary">
+        npm install @nextui-org/react
+      </Snippet>
+    </div>
+  );
+}
